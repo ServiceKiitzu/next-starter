dc3c779b7040d8fbb926e8d3d340e95c96c4b604
Frank Liao <frank880703@gmail.com>
2022-11-26 23:39:46 +0800

    feat: scss public variable

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/variables/index.scss";`,
 	},
 	i18n: {
 		locales: ['zh', 'en'],
